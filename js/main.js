210dc80a66d50914ef6794cc6b4218b8c27c9351
jonfur <flyingspacelion@gmail.com>
2016-04-01 18:28:10 -0400

    add fade in fade out functionality, change layout

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//Helper functions
+/*Helper functions*/
 function $q (cssSelector) {
   return document.querySelector(cssSelector);
 };
@@ -6,69 +6,74 @@ function $el (element) {
   return document.createElement(element);
 };
 
-//Cached Nodes
+
+/*Cached Nodes*/
 var inputField = $q("#input-field");
 var displayH1 = $q("#display-h1");
 var body = document.body;
 var reverseButton = $q("#reverse-button");
+var displayOpacity = displayH1.style.opacity;
 
-//String reversal function
+
+/*String reversal function*/
 function reverse (s) {
   return s.split('').reverse().join('');
 }
 
 
-//Call reversal function on button click
+/*Button click swap opacity value and call reverse function on #input-field
+value*/
 function clickHandler () {
-  displayH1.textContent = reverse(inputField.value);
-  opacityShow();
+  opacitySwitch();
+};
+
+
+/*Check opacity value and fade in or out depending on value*/
+function opacitySwitch () {
+  if (displayH1.style.opacity == 1) {
+    fadeOut();
+  }
+  else {
+    fadeIn();
+  }
+};
+
+/*change opacity to 0 from 1, triggering transition, resulting in fade out
+effect. add listener to displayH1. see below for details*/
+function fadeOut () {
+  displayH1.style.opacity = 0;
+  addListener();
 }
 
-//Playing with opacity
 
-function eraseH1() {
-  displayH1.textContent = "";
-}
+/*change opacity to 1 from 0, triggering transition, resulting in fade in
+effect. also call reverse function on input-field value*/
+function fadeIn () {
+  displayH1.style.opacity = 1;
+  writeH1();
+};
 
+
+/*write reversed input-field value to displayH1*/
 function writeH1 () {
   displayH1.textContent = reverse(inputField.value);
+};
+
+
+/*Listeners*/
+
+function addListener () {
+  displayH1.addEventListener("transitionend", transHandler);
 }
 
-function opacityReset () {
-  displayH1.style.opacity = 0;
-}
-
-function opacityShow () {
-  displayH1.style.opacity = 1;
-}
-
-function testTrans (event) {
-  var toots = event;
-  console.log(toots);
-}
-
-function opacitySwitch () {
-  if (displayH1.style.opacity === 1) {
-
-  }
-}
-
-//Attempting timeOuts
-var timeoutID;
-
-function delayFade () {
-  timeoutID = window.setTimeout(opacityReset, 300);
-}
-
-function fadeInOut () {
-  if (displayH1.style.opacity === 1) {
-
-  }
-}
-
-
-/*Add enter button functionality*/
+/*Click functionality for button (Add enter button functionality later)*/
 reverseButton.addEventListener("click", clickHandler);
 
-//Transition event listener on h1 **Not Working**
-displayH1.addEventListener("transitioned", testTrans);
+
+
+/*Called by addListener*/
+function transHandler () {
+  console.log("yay");
+  fadeIn();
+  displayH1.removeEventListener("transitionend", transHandler);
+}
